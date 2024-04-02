515fb3b8072e8cd77012073b972f05af08532945
gabeaguilar <gaa2292@utexas.edu>
2024-04-01 20:19:42 -0500

    Merge

--- UnsortedSet.java
+++ UnsortedSet.java
@@ -16,61 +16,83 @@
  *  email address:   
  */
 
-import java.util.Iterator;
-import java.util.ArrayList;
-
-/**
- * A simple implementation of an ISet.
- * Elements are not in any particular order.
- * Students are to implement methods that
- * were not implemented in AbstractSet and override
- * methods that can be done more efficiently.
- * An ArrayList must be used as the internal storage container.
- *
- */
-public class UnsortedSet<E> extends AbstractSet<E> {
-
-    private ArrayList<E> myCon;
-
-
-    public UnsortedSet(){
-        myCon = new ArrayList<>();
-    }
-    @Override
-    public boolean add(E item) {
-        // TODO Auto-generated method stub
-        return true;
-    }
-
-    @Override
-    public ISet<E> difference(ISet<E> otherSet) {
-        // TODO Auto-generated method stub
-        return new UnsortedSet<>();
-    }
-
-    @Override
-    public Iterator<E> iterator() {
-        return myCon.iterator();
-    }
-
-    @Override
-    public ISet<E> union(ISet<E> otherSet) {
-        return new UnsortedSet<>();
-    }
-
-    @Override
-    public void clear() {
-        myCon.clear();
-    }
-
-    @Override
-    public ISet<E> intersection(ISet<E> otherSet) {
-        return new UnsortedSet<>();
-    }
-
-    @Override
-    public int size() {
-        return myCon.size();
-    }
-
-}
+ import java.util.Iterator;
+ import java.util.ArrayList;
+ 
+ /**
+  * A simple implementation of an ISet. 
+  * Elements are not in any particular order.
+  * Students are to implement methods that 
+  * were not implemented in AbstractSet and override
+  * methods that can be done more efficiently. 
+  * An ArrayList must be used as the internal storage container.
+  *
+  */
+ public class UnsortedSet<E> extends AbstractSet<E> {
+ 
+     private ArrayList<E> myCon; //internal storage container (underlying Data Structure)
+ 
+     public UnsortedSet() {
+         myCon = new ArrayList<>();
+     }
+ 
+     public UnsortedSet(ISet<E> otherSet) {
+         if (otherSet == null) {
+             throw new IllegalArgumentException("Other set must not be null.");
+         }
+         myCon = new ArrayList<>();
+         Iterator<E> it = this.iterator();
+         while (it.hasNext()) {
+             this.add(it.next());
+         }
+     }
+ 
+     public boolean add(E item) {
+         if(!contains(item)) {
+             myCon.add(item);
+             return true;
+         } else {
+             return false;
+         }
+     }
+ 
+     public void clear() {
+         myCon.clear();
+     }
+ 
+     public boolean difference(E item) {
+         return true;
+     }
+ 
+     public ISet<E> union(ISet<E> otherSet) {
+         ISet<E> result = new UnsortedSet<>(otherSet);
+         Iterator<E> it = this.iterator();
+         while(it.hasNext()) {
+             E element = it.next();
+             if(!otherSet.contains(element)) {
+                 result.add(element);
+             }
+         }
+         return result;
+     }
+ 
+     public ISet<E> intersection(ISet<E> otherSet) {
+         ISet<E> result = new UnsortedSet<>();
+         Iterator<E> it = this.iterator();
+         while(it.hasNext()) {
+             E element = it.next();
+             if(otherSet.contains(element)) {
+                 result.add(element);
+             }
+         }
+         return result;
+     }
+ 
+     public int size() {
+         return myCon.size();
+     }
+ 
+ 
+ 
+ }
+ 
